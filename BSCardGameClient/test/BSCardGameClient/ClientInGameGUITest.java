e9762d985c503e157fe9843e1db5a98967808134
alintawang2 <47797219+alintawang2@users.noreply.github.com>
2019-04-28 22:43:06 -0500

    readded GameTest, added ClientInGameTest

---
+++ BSCardGameClient/test/BSCardGameClient/ClientInGameGUITest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BSCardGameClient;
+
+import bscardgameclient.ClientInGameGUI;
+import bscardgameclient.ClientInGameGUI.setLobbyPort;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Alinta Wang
+ */
+public class ClientInGameGUITest {
+    
+    @Test
+    public void setLobbyPortTest(){
+        setLobbyPort(5);
+    }
+
+   
+}
